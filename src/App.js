f24ef25a27e58f4b865663b6b877d00b00376c3e
cristian suarez <casm2203@gmail.com>
2022-10-07 22:33:18 -0500

    consumiendo api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [pagination, setPagination] = useState(1);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/photos?_page=${pagination}&_limit=4`
+      );
+      const data = await response.json();
+      setPhotos(data);
+    };
+    getPhotos();
+  }, [pagination]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="row mt-3">
+        <div className="col shadow-sm p-3 mb-5 bg-body rounded">
+          <div className="container">
+            <div className="row">
+              {photos.map((photo) => (
+                <div className="col-3 mb-1" key={photo.id}>
+                  <div className="card w-100">
+                    <div className="card-body">
+                      <img
+                        src={photo.thumbnailUrl}
+                        className="card-img-top"
+                        alt="..."
+                      />
+                      <strong>Titulo de la foto:</strong>
+                      <h6 className="card-title text-capitalize">
+                        {photo.title}
+                      </h6>
+                      <br></br>
+                      <p className="card-text txt-sz text-capitalize">
+                        <strong>Pagina del fotografo:</strong> {photo.url}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className="container-fluid d-flex justify-content-center">
+              <nav aria-label="Page navigation example">
+                <ul className="pagination">
+                  <li className="page-item">
+                    <button className="page-link" href="/">
+                      Pagina anterior
+                    </button>
+                  </li>
+                  <li className="page-item">
+                    <button className="page-link" href="/">
+                      Pagina siguiente
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
